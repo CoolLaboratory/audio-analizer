4566acf9b0ee972f81d384c444396d76c92eef34
Cool Laboratory <cool.lab.gr@gmail.com>
2021-03-03 00:13:38 +0200

    Create loading scrim component

---
+++ src/LoadingScrim.svelte
@@ -0,0 +1,20 @@
+<div class="loading-scrim">
+
+<h1>Loading...</h1>
+
+</div>
+
+<style>
+
+  .loading-scrim {
+    position: absolute;
+    display: flex;
+    background-color: black;
+    align-items: center;
+    justify-content: center;
+    top: 0; right: 0;
+    bottom: 0; left: 0;
+    z-index: 2;
+  }
+
+</style>
