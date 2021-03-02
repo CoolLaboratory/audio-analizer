e8ec871a09bd6d89c762df5d4735ef4f8a76e431
Cool Laboratory <cool.lab.gr@gmail.com>
2021-03-03 00:13:05 +0200

    fix bugs

--- src/Analizer.svelte
+++ src/Analizer.svelte
@@ -1,159 +1,164 @@
 <script>
-	import {onMount, tick} from 'svelte'
-	
+	import { onMount, tick } from "svelte";
+
 	/**@type {AudioContext} */
 	export let audioCtx;
+
+	/**@type {MediaElementAudioSourceNode}*/
 	export let source;
-	export let visual = 'sinewave'
-	
-	let canvas
-	let canvasCtx
-	let drawVisual
-	
+
+	/**@type {string}*/
+	export let visual = "sinewave";
+
+	/**@type {HTMLCanvasElement}*/
+	let canvas;
+
+	/**@type {CanvasRenderingContext2D}*/
+	let canvasCtx;
+
+	let animFrame;
+
 	const analyser = audioCtx.createAnalyser();
 	analyser.minDecibels = -90;
 	analyser.maxDecibels = -10;
 	analyser.smoothingTimeConstant = 0.85;
-	
-	$: visualSetting = visual
-	
-	onMount(async () => {
-		await tick()
-		canvasCtx = canvas.getContext("2d");
+
+	$: visualSetting = visual;
+	$: if (canvas) canvasCtx = canvas.getContext("2d");
+
+	let isPlaying;
+	$: if (isPlaying) {
+		if(visual) {
+			stop()
+			start()
+		}
+	}
+
+	$: if (source) {
 		source.connect(analyser);
-		
-		source.mediaElement.onplay = (e) => {
-			console.log('isPlaying')
-			visualize()
-		}
-		
-		source.mediaElement.onpause = (e) => {
-			console.log('isPaused')
-			window.cancelAnimationFrame(drawVisual)
-		}
 
-		console.log(source)
-		
-	})
-	
+		source.mediaElement.onplay = () => start();
+		source.mediaElement.onpause = () => stop();
+		source.mediaElement.onended = () => stop();
+	}
+
+	const start = () => {
+		console.log("starting");
+		isPlaying = true;
+		visualize();
+	};
+
+	const stop = () => {
+		isPlaying = false;
+		window.cancelAnimationFrame(animFrame);
+	};
+
 	function visualize() {
-		const WIDTH = canvas.width = window.innerWidth;
-		const HEIGHT = canvas.height = window.innerHeight;
-		
-		const options = { 
-			WIDTH, 
-			HEIGHT 
-		}
+		const WIDTH = (canvas.width = window.innerWidth);
+		const HEIGHT = (canvas.height = window.innerHeight);
 
+		const options = {
+			WIDTH,
+			HEIGHT,
+		};
 
-		if(visualSetting == "sinewave") {
+		if (visualSetting == "sinewave") {
 			buildSineWave(options);
-		} 
-		else if(visualSetting == "frequencybars") {
-			buildFrequencyBars(options)
-		} 
-		else if(visualSetting == "off") {
+		} else if (visualSetting == "frequencybars") {
+			buildFrequencyBars(options);
+		} else if (visualSetting == "off") {
 			canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 			canvasCtx.fillStyle = "red";
 			canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 		}
 	}
 
-	function buildSineWave({
-		WIDTH, 
-		HEIGHT
-	}) {
+	function buildSineWave({ WIDTH, HEIGHT }) {
 		analyser.fftSize = 1024;
-    var bufferLength = analyser.fftSize;
-    console.log(bufferLength);
-    var dataArray = new Float32Array(bufferLength);
+		var bufferLength = analyser.fftSize;
+		console.log(bufferLength);
+		var dataArray = new Float32Array(bufferLength);
 
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+		canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    function draw() {
+		function draw() {
+			animFrame = requestAnimationFrame(draw);
 
-      drawVisual = requestAnimationFrame(draw);
+			analyser.getFloatTimeDomainData(dataArray);
 
-      analyser.getFloatTimeDomainData(dataArray);
+			canvasCtx.fillStyle = "rgb(0, 0, 0)";
+			canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+			canvasCtx.lineWidth = 2;
+			canvasCtx.strokeStyle = "rgb(0, 200, 200)";
 
-      canvasCtx.lineWidth = 2;
-      canvasCtx.strokeStyle = 'rgb(0, 200, 200)';
+			canvasCtx.beginPath();
 
-      canvasCtx.beginPath();
+			var sliceWidth = (WIDTH * 1.0) / bufferLength;
+			var x = 0;
 
-      var sliceWidth = WIDTH * 1.0 / bufferLength;
-      var x = 0;
+			for (var i = 0; i < bufferLength; i++) {
+				var v = dataArray[i] * 200.0;
+				var y = HEIGHT / 2 + v;
 
-      for(var i = 0; i < bufferLength; i++) {
-   
-        var v = dataArray[i] * 200.0;
-        var y = HEIGHT/2 + v;
+				if (i === 0) {
+					canvasCtx.moveTo(x, y);
+				} else {
+					canvasCtx.lineTo(x, y);
+				}
 
-        if(i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
+				x += sliceWidth;
+			}
 
-        x += sliceWidth;
-      }
+			canvasCtx.lineTo(canvas.width, canvas.height / 2);
+			canvasCtx.stroke();
+		}
 
-      canvasCtx.lineTo(canvas.width, canvas.height/2);
-      canvasCtx.stroke();
-    };
-
-    draw();
+		draw();
 	}
-	
-	function buildFrequencyBars({
-		WIDTH, 
-		HEIGHT
-	}) {
+
+	function buildFrequencyBars({ WIDTH, HEIGHT }) {
 		analyser.fftSize = 256;
-    var bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
-    var dataArray = new Float32Array(bufferLength);
+		var bufferLength = analyser.frequencyBinCount;
+		console.log(bufferLength);
+		var dataArray = new Float32Array(bufferLength);
 
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+		canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    function draw() {
-      drawVisual = requestAnimationFrame(draw);
+		function draw() {
+			animFrame = requestAnimationFrame(draw);
 
-      analyser.getFloatFrequencyData(dataArray);
+			analyser.getFloatFrequencyData(dataArray);
 
-      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+			canvasCtx.fillStyle = "rgb(0, 0, 0)";
+			canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      var barWidth = (WIDTH / bufferLength) * 2.5;
-      var barHeight;
-      var x = 0;
+			var barWidth = (WIDTH / bufferLength) * 2.5;
+			var barHeight;
+			var x = 0;
 
-      for(var i = 0; i < bufferLength; i++) {
-        barHeight = (dataArray[i] + 140)*3;
-        
-        canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight+100) + ',50,50)';
-        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+			for (var i = 0; i < bufferLength; i++) {
+				barHeight = (dataArray[i] + 140) * 3;
 
-        x += barWidth + 1;
-      }
-    };
+				canvasCtx.fillStyle = "rgb(" + Math.floor(barHeight + 100) + ",50,50)";
+				canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
 
-    draw();
+				x += barWidth + 1;
+			}
+		}
+
+		draw();
 	}
-	
-	function onresize(e) {
-		console.log('window resized.')
 
-		window.cancelAnimationFrame(drawVisual);
-  	visualize();
+	function onresize(e) {
+		console.log("window resized.");
+		stop()
+		start()
 	}
 </script>
 
 <svelte:window on:resize={onresize} />
-<canvas bind:this="{canvas}"></canvas>
+<canvas bind:this={canvas} />
 
 <style>
 	canvas {
